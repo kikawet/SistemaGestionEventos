980b5ac38869cbf2088e2de1e3728c308aee0134
Francisco Jesús Najas Martos <fjnm0006@red.ujaen.es>
2020-05-08 13:44:57 +0200

    Creacion de las entidades

--- src/main/java/equipo3/ujaen/backend/sistemagestioneventos/entidades/Evento.java
+++ src/main/java/equipo3/ujaen/backend/sistemagestioneventos/entidades/Evento.java
@@ -1,5 +1,87 @@
 package equipo3.ujaen.backend.sistemagestioneventos.entidades;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Evento {
 
+	
+	public enum TipoEvento {
+		BENEFICO,
+		NO_BENEFICO
+	}
+	
+	private String lugar;
+	private Date fecha;
+	private TipoEvento tipoEvento;
+	private String descripcion;
+	private int aforoMaximo;
+	private String idEvento;
+	
+	private List<Usuario> asistentes;
+	private List<Usuario> listaEspera;
+	
+	public Evento() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Evento(String lugar, Date fecha, TipoEvento tipoEvento, String descripcion, int aforoMaximo,
+			String idEvento) {
+		super();
+		this.lugar = lugar;
+		this.fecha = fecha;
+		this.tipoEvento = tipoEvento;
+		this.descripcion = descripcion;
+		this.aforoMaximo = aforoMaximo;
+		this.idEvento = idEvento;
+		
+		this.asistentes = new ArrayList<>();
+		this.listaEspera = new ArrayList<>();
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getLugar() {
+		return lugar;
+	}
+
+	public TipoEvento getTipoEvento() {
+		return tipoEvento;
+	}
+
+	public int getAforoMaximo() {
+		return aforoMaximo;
+	}
+
+	public String getIdEvento() {
+		return idEvento;
+	}
+
+	public List<Usuario> getAsistentes() {
+		return asistentes;
+	}
+
+	public List<Usuario> getListaEspera() {
+		return listaEspera;
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/equipo3/ujaen/backend/sistemagestioneventos/entidades/Usuario.java
+++ src/main/java/equipo3/ujaen/backend/sistemagestioneventos/entidades/Usuario.java
@@ -1,5 +1,46 @@
 package equipo3.ujaen.backend.sistemagestioneventos.entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
 
+	private String login;
+	private String password;
+
+	private List<Evento> eventosCreados;
+	
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Usuario(String login, String password) {
+		super();
+		this.login = login;
+		this.password = password;
+		
+		this.eventosCreados = new ArrayList<>();
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public List<Evento> getEventosCreados() {
+		return eventosCreados;
+	}
+	
+	
+	
 }
