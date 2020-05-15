40aa371ddc5f041bb7cee443ce19708a5eb0233e
flo00008 <flo00008@red.ujaen.es>
2020-05-15 10:43:33 +0200

    Creado SpringTests

---
+++ src/test/java/equipo3/ujaen/backend/sistemagestioneventos/beans/SistemaGestionEventosIntegrationTest.java
@@ -0,0 +1,14 @@
+package equipo3.ujaen.backend.sistemagestioneventos.beans;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import equipo3.ujaen.backend.sistemagestioneventos.interfaces.InterfaceSistemaGestionEventos;
+
+@SpringBootTest(classes = { SistemaGestionEventos.class })
+public class SistemaGestionEventosIntegrationTest {
+
+	@Autowired
+	static InterfaceSistemaGestionEventos gestorEventos;
+
+}
