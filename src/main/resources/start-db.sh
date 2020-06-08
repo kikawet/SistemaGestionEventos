b636f6da5c442cc2ab6f82a9390d390f61720558
Francisco López Ortega <flo00008@red.ujaen.es>
2020-06-08 14:03:00 +0200

    Abre el puerto del contenedor

--- src/main/resources/start-db.sh
+++ src/main/resources/start-db.sh
@@ -1,3 +1,3 @@
-docker run --name sge-db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=sge -e MYSQL_USER=sge_user -e MYSQL_PASSWORD=javaee -d mysql
+docker run -p 3306:3306 --name sge-db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=sge -e MYSQL_USER=sge_user -e MYSQL_PASSWORD=javaee -d mysql
 
 echo "para conectarte desde la consola del contenedor ejecuta: `mysql -usge_user -pjavaee`"
