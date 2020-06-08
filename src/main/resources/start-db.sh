fdcecd1399b1d4e16d2e446bcfa26d889c1ae406
Francisco López Ortega <flo00008@red.ujaen.es>
2020-06-08 13:52:47 +0200

    bd con usuario y contraseña

--- src/main/resources/start-db.sh
+++ src/main/resources/start-db.sh
@@ -1,3 +1,3 @@
-docker run --name sge-db -e MYSQL_ROOT_PASSWORD=javaee -e MYSQL_DATABASE=sge -d mysql
+docker run --name sge-db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=sge -e MYSQL_USER=sge_user -e MYSQL_PASSWORD=javaee -d mysql
 
 echo "para conectarte desde la consola del contenedor ejecuta: `mysql -uroot -pjavaee`"
