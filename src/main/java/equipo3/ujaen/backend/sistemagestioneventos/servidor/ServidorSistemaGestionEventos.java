aa2ae857941cb07cac4df87dc2e2220c02084887
flo00008 <flo00008@red.ujaen.es>
2020-06-16 09:59:26 +0200

    Añadiendo cache de BD

--- src/main/java/equipo3/ujaen/backend/sistemagestioneventos/servidor/ServidorSistemaGestionEventos.java
+++ src/main/java/equipo3/ujaen/backend/sistemagestioneventos/servidor/ServidorSistemaGestionEventos.java
@@ -3,11 +3,13 @@ package equipo3.ujaen.backend.sistemagestioneventos.servidor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "equipo3.ujaen.backend.sistemagestioneventos.beans")
 @EntityScan(basePackages = "equipo3.ujaen.backend.sistemagestioneventos.entidades")
 @EnableJpaRepositories(basePackages = "equipo3.ujaen.backend.sistemagestioneventos.dao")
+@EnableCaching
 public class ServidorSistemaGestionEventos {
 
 	public static void main(String[] args) {
