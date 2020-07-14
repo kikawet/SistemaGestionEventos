804c4b62a25a7f8b1d0146b502011def731efa9a
ajml0012 <ajml0012@red.ujaen.es>
2020-07-14 12:09:01 +0200

    Carga de eventos en controlador Inicio

--- src/main/java/equipo3/ujaen/backend/sistemagestioneventos/webccontroller/InicioController.java
+++ src/main/java/equipo3/ujaen/backend/sistemagestioneventos/webccontroller/InicioController.java
@@ -1,15 +1,27 @@
 package equipo3.ujaen.backend.sistemagestioneventos.webccontroller;
 
+
+
+
+
+import java.util.List;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ch.qos.logback.classic.Logger;
+import equipo3.ujaen.backend.sistemagestioneventos.dtos.EventoDTO;
+import equipo3.ujaen.backend.sistemagestioneventos.entidades.Evento;
+import equipo3.ujaen.backend.sistemagestioneventos.entidades.Usuario;
 import equipo3.ujaen.backend.sistemagestioneventos.interfaces.InterfaceSistemaGestionEventos;
 
 @Controller
-@RequestMapping("/inicio")
+@RequestMapping("/")
 public class InicioController {
 
 	private static final Logger log = (Logger) LoggerFactory.getLogger(InicioController.class);
@@ -17,4 +29,16 @@ public class InicioController {
 	@Autowired
 	InterfaceSistemaGestionEventos ige;
 
+	private void initController (WebDataBinder web) {
+		log.info("Iniciando controlador!!");
+	}
+	
+	@GetMapping(path = {"/"})
+	public String listado(ModelMap model) {
+			
+	        List<EventoDTO> eventos=ige.listarEventos(null, "", 0,10);	        
+	        model.addAttribute("eventos", eventos);
+	        log.info("Cargando eventos!!"+eventos.size());
+	        return "index";
+	}
 }
