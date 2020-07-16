028fad03b7a71a51c1c6002f5717a099fe6deb0e
flo00008 <flo00008@red.ujaen.es>
2020-07-16 19:37:12 +0200

    Añadida bsuqueda por titulo

--- src/main/java/equipo3/ujaen/backend/sistemagestioneventos/webccontroller/InicioController.java
+++ src/main/java/equipo3/ujaen/backend/sistemagestioneventos/webccontroller/InicioController.java
@@ -1,16 +1,11 @@
 package equipo3.ujaen.backend.sistemagestioneventos.webccontroller;
 
-
-
-
-
 import java.util.List;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,42 +15,39 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import ch.qos.logback.classic.Logger;
 import equipo3.ujaen.backend.sistemagestioneventos.dtos.EventoDTO;
-import equipo3.ujaen.backend.sistemagestioneventos.entidades.Evento;
-import equipo3.ujaen.backend.sistemagestioneventos.entidades.Usuario;
 import equipo3.ujaen.backend.sistemagestioneventos.interfaces.InterfaceSistemaGestionEventos;
 
 @Controller
 @RequestMapping("/")
 public class InicioController {
 
-	private static final Logger log = (Logger) LoggerFactory.getLogger(InicioController.class);
+    private static final Logger log = (Logger) LoggerFactory.getLogger(InicioController.class);
 
-	@Autowired
-	InterfaceSistemaGestionEventos ige;
+    @Autowired
+    InterfaceSistemaGestionEventos ige;
 
-//	private void initController (WebDataBinder web) {
-//		log.info("Iniciando controlador!!");
-//	}
-	
-	@GetMapping(path = {"/"})
-	public String listado(ModelMap model,@ModelAttribute("busqueda")String filtro) {
-			//String filtro=(String)model.getAttribute("busqueda");
-		log.info("filtro: "+filtro);
-			if(filtro==null) {
-				
-				filtro="";
-			}
-	        List<EventoDTO> eventos=ige.listarEventos(null, filtro, 0,10);	        
-	        model.addAttribute("eventos", eventos);
-	        log.info("Cargando eventos!!"+eventos.size());
-	        return "index";
+    // private void initController (WebDataBinder web) {
+    // log.info("Iniciando controlador!!");
+    // }
+
+    @GetMapping(path = { "/" })
+    public String listado(ModelMap model, @ModelAttribute("busqueda") String tituloParcial) {
+
+	if (tituloParcial == null) {
+	    tituloParcial = "";
 	}
-	
-	@PostMapping
-	public String busqueda(@RequestParam(value = "buscarNombre")String busqueda,RedirectAttributes redirect) {
-		
-		redirect.addFlashAttribute("busqueda", busqueda);
-		
-		return "redirect:/";
-	}
+
+	List<EventoDTO> eventos = ige.listarEventos(null, "", tituloParcial, 0, 10);
+	model.addAttribute("eventos", eventos);
+	log.info("Cargando eventos!!" + eventos.size());
+	return "index";
+    }
+
+    @PostMapping
+    public String busqueda(@RequestParam(value = "buscarNombre") String busqueda, RedirectAttributes redirect) {
+
+	redirect.addFlashAttribute("busqueda", busqueda);
+
+	return "redirect:/";
+    }
 }
