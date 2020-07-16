aa07ae40eff297669d809e039a642a0424764204
flo00008 <flo00008@red.ujaen.es>
2020-07-17 00:03:10 +0200

    Añadido creador

--- src/main/java/equipo3/ujaen/backend/sistemagestioneventos/webccontroller/EventoController.java
+++ src/main/java/equipo3/ujaen/backend/sistemagestioneventos/webccontroller/EventoController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import ch.qos.logback.classic.Logger;
 import equipo3.ujaen.backend.sistemagestioneventos.dtos.EventoDTO;
-import equipo3.ujaen.backend.sistemagestioneventos.dtos.UsuarioDTO;
+import equipo3.ujaen.backend.sistemagestioneventos.dtos.UsuarioDTODetails;
 import equipo3.ujaen.backend.sistemagestioneventos.excepciones.EventoPrescrito;
 import equipo3.ujaen.backend.sistemagestioneventos.excepciones.EventoYaRegistrado;
 import equipo3.ujaen.backend.sistemagestioneventos.interfaces.InterfaceSistemaGestionEventos;
@@ -23,42 +24,41 @@ import equipo3.ujaen.backend.sistemagestioneventos.interfaces.InterfaceSistemaGe
 @RequestMapping("/evento")
 public class EventoController {
 
-	private static final Logger log = (Logger) LoggerFactory.getLogger(InicioController.class);
+    private static final Logger log = (Logger) LoggerFactory.getLogger(InicioController.class);
 
-	@Autowired
-	InterfaceSistemaGestionEventos ige;
+    @Autowired
+    InterfaceSistemaGestionEventos ige;
 
-	@RequestMapping(value = "/crea", method = RequestMethod.GET)
-	public String crearEvento(ModelMap model) {
+    @RequestMapping(value = "/crea", method = RequestMethod.GET)
+    public String crearEvento(ModelMap model) {
 
-		log.info("get - crearEvento");
-		model.addAttribute("evento", new EventoDTO());
-		return "crearEvento";
+	log.info("get - crearEvento");
+	model.addAttribute("evento", new EventoDTO());
+	return "crearEvento";
+    }
+
+    @PostMapping("/crea")
+    public String crearEvento(@ModelAttribute("evento") @Valid EventoDTO eventoDTO, BindingResult result,
+	    @AuthenticationPrincipal UsuarioDTODetails principal) {
+
+	log.info("post - registro");
+	String view = "redirect:/";
+
+	// log.info(eventoDTO.getFecha().toString());
+	if (!result.hasErrors()) {
+	    try {
+		ige.crearEventoPorUsuario(principal.getUsuario(), eventoDTO, false);
+	    } catch (EventoYaRegistrado eyr) {
+		result.rejectValue("titulo", "error.evento.crea", "Ya existe este evento");
+		view = "crearEvento";
+	    } catch (EventoPrescrito ep) {
+		result.rejectValue("fecha", "error.evento.crea2", "Evento preescrito");
+		view = "crearEvento";
+	    }
+	} else {
+	    view = "crearEvento";
 	}
 
-	@PostMapping("/crea")
-	public String crearEvento(@ModelAttribute("evento") @Valid EventoDTO eventoDTO, BindingResult result) {
-		log.info("post - registro");
-		String view = "redirect:/";
-		// Hay que quitar esto luego
-		UsuarioDTO patricio = new UsuarioDTO("Patricio Ruiz", "1234");
-
-		//log.info(eventoDTO.getFecha().toString());
-		if (!result.hasErrors()) {
-			try {
-				ige.crearEventoPorUsuario(patricio, eventoDTO, false);
-			} catch (EventoYaRegistrado eyr) {
-				result.rejectValue("titulo", "error.evento.crea", "Ya existe este evento");
-				view = "crearEvento";
-			} catch (EventoPrescrito ep) {
-				result.rejectValue("fecha", "error.evento.crea2", "Evento preescrito");
-				view = "crearEvento";
-			}
-		} else {
-			view = "crearEvento";
-		}
-		
-		
-		return view;
-	}
+	return view;
+    }
 }
